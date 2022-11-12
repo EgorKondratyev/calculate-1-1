f53076d90a956aeb582ec85ecb5b5ebdda2fb914
Egor Kondratyev <zhora_kondratev@bk.ru>
2022-11-12 13:19:14 +0300

    what...

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from logger.create_logger import logger
 
 
-def foo(first_num: int = 0, second_num: int = 0) -> int:
-    return first_num + second_num
+def foo(first_num: int = 0, second_num: int = 0) -> str:
+    return str(first_num) + str(second_num)
 
 
 if __name__ == '__main__':
